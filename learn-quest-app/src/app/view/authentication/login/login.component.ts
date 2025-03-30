a13c98adb798b8c231ee23b3b00baefc4edcd1bc
NandoJans <nandojans@gmail.com>
2025-03-30 12:29:02 +0200

    1-login-page Login Page

--- learn-quest-app/src/app/view/authentication/login/login.component.ts
+++ learn-quest-app/src/app/view/authentication/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {PrimaryButtonComponent} from '../../../components/buttons/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    PrimaryButtonComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usernameControl: FormControl = new FormControl('', Validators.required);
+  passwordControl: FormControl = new FormControl('', Validators.required);
 
+  loginForm: FormGroup = new FormGroup({
+    username: this.usernameControl,
+    password: this.passwordControl
+  });
+
+  getInnerBorderColor() {
+    if (this.loginForm.invalid) {
+      return 'border-color-1'
+    } else {
+      return 'border-color-2'
+    }
+  }
+
+  getOuterBorderColor() {
+    if (this.loginForm.invalid) {
+      return 'border-color-5'
+    } else {
+      return 'border-color-3'
+    }
+  }
+
+  submit() {
+    if (this.loginForm.invalid) return;
+
+    console.log('username:', this.usernameControl.value);
+    console.log('password:', this.passwordControl.value)
+  }
 }
