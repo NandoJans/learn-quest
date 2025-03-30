f7bce605151bfd06bc7d260f378e651795294d69
NandoJans <nandojans@gmail.com>
2025-03-30 11:31:32 +0200

    1-login-page Login page view

--- learn-quest-app/src/app/app.routes.ts
+++ learn-quest-app/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './view/authentication/login/login.component';
 
 export const routes: Routes = [
-  {}
+  {component: LoginComponent, path: '', title: 'Login'},
+  {component: LoginComponent, path: 'login', title: 'Login'},
 ];
