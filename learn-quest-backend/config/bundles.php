9f6033b84eab810d7dcacc9ee572d17895a9aa8f
NandoJans <nandojans@gmail.com>
2025-07-23 19:04:04 +0200

    1-login-page CORS fixes

--- learn-quest-backend/config/bundles.php
+++ learn-quest-backend/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
