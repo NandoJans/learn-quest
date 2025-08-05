49a4f1f41b06e6cf90b31829f30f2e76ecefe6e9
nando <nandojans@gmail.com>
2025-08-05 20:48:50 +0200

    try to get id from token

--- learn-quest-app/src/app/services/course.service.ts
+++ learn-quest-app/src/app/services/course.service.ts
@@ -28,9 +28,13 @@ export class CourseService {
   }
 
   enrollInCourse(courseId: number): Observable<any> {
+    const userId = this.securityService.getUser()?.id;
+    if (!userId) {
+      throw new Error('User must be logged in to enroll in a course');
+    }
     const body = {
       course_id: courseId,
-
+      user_id: userId
     }
     return this.apiService.post<any>(`course/enroll`, body);
   }
