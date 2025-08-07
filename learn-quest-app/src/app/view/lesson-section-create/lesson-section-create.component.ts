8709396d203253fcabecc9246c392a7e160223b0
NandoJans <90688887+NandoJans@users.noreply.github.com>
2025-08-07 20:17:26 +0200

    style: add bootstrap layout to lesson section create

--- learn-quest-app/src/app/view/lesson-section-create/lesson-section-create.component.ts
+++ learn-quest-app/src/app/view/lesson-section-create/lesson-section-create.component.ts
@@ -1,16 +1,19 @@
-import {Component} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {Component, ViewChild} from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 import {LessonSection} from '../../entities/lesson-section';
 import {LessonSectionService} from '../../services/lesson-section.service';
+import {PrimaryButtonComponent} from '../../components/buttons/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-lesson-section-create',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, PrimaryButtonComponent],
   templateUrl: './lesson-section-create.component.html',
   styleUrl: './lesson-section-create.component.css'
 })
 export class LessonSectionCreateComponent {
   section: LessonSection = new LessonSection();
+  @ViewChild('sectionForm') sectionForm?: NgForm;
 
   constructor(private lessonSectionService: LessonSectionService) {}
 
@@ -20,5 +23,29 @@ export class LessonSectionCreateComponent {
         this.section = new LessonSection();
       }
     });
+  }
+
+  getInnerBorderColor() {
+    if (!this.sectionForm) {
+      return 'border-color-2';
+    }
+
+    if (this.sectionForm.invalid && (this.sectionForm.dirty || this.sectionForm.touched)) {
+      return 'border-color-1';
+    }
+
+    return 'border-color-2';
+  }
+
+  getOuterBorderColor() {
+    if (!this.sectionForm) {
+      return 'border-color-3';
+    }
+
+    if (this.sectionForm.invalid && (this.sectionForm.dirty || this.sectionForm.touched)) {
+      return 'border-color-5';
+    }
+
+    return 'border-color-3';
   }
 }
